84a9470160046fb6990e49ac7a08bb32865df53e
olgashchekina <olya6avg@gmail.com>
2020-01-14 16:16:59 -0800

    add tests

--- test/specs/register.js
+++ test/specs/register.js
@@ -1,8 +1,8 @@
 
 const assert = require('assert');
 const {expect} = require ('chai');
-const email = Math.random()+'olya6avg@gmail.com';
-const firstName = 'John';
+//const email = 'www@gmail.com'//Math.random()+'olya6avg@gmail.com';
+//const firstName = 'John';
 
 describe('Register page', () => {
     it('should have the right title', () => {
@@ -22,14 +22,9 @@ describe('Register page', () => {
         const expected = 'Profiles with fictitious or dummy data will be deleted.';
         expect (actual).equal(expected);
     });
-    it('should have a correct submit button text', () => {
-        const actual = $('form button').getText();
-        const expected = 'Submit';
-        expect (actual).equal(expected);
-    });
     it('should fill first name field', () => {
         const element = $('form input[name="firstName"]');
-        element.setValue(firstName);
+        element.setValue('John');
     });
     it('should fill last name field', () => {
         const element = $('form input[name="lastName"]');
@@ -37,32 +32,66 @@ describe('Register page', () => {
     });
     it('should fill  email field', () => {
         const element = $('form input[name="email"]');
-        element.setValue(email);
+        element.setValue('aaa@gmail.com');
     });
-    it('should fill passworn field', () => {
+    it('should fill  Cell phone numberfield', () => {
+        const element = $('form input[name="phone"]');
+        element.setValue('16503055577');
+    });
+    it('should fill password field', () => {
         const element = $('form input[name="password"]');
-        element.setValue('wrereerrg');
+        element.setValue('11111');
 
     });
     it('should fill up about field', () => {
         const element = $('form textarea[name="about"]');
         element.setValue('about me');
-
     });
+
     it('should fill goals field', () => {
         const element = $('form textarea[name="goals"]');
-        element.setValue('wrereerrg');
-    });
+        element.setValue('hello');
+   });
     it('should choose English level from dropdown menu', () => {
         const element = $('form select[name="englishLevel"]');
         element.selectByVisibleText('Native');
+    });
+
+    it('should choose click button', () => {
+        const element = $('form button[type="submit"]').click();
+        //element.click();
 
     });
-    it('should click button submit', () => {
-        const element = $('form button[type="submit"]');
-        element.click();
-        browser.pause(2000);
+    it('should have the right login title', () => {
+        browser.url('https://stage.pasv.us/user/login');
+        const actual = browser.getTitle();
+        const expected = 'Progress Monitor';
+        expect (actual).equal(expected);
     });
+    it('should have a correct title ', () => {
+        const actual = $('h1').getText();
+        const expected = 'User Login';
+        expect (actual).equal(expected);
+    });
+
+    it('should fill Email field on Login page', () => {
+        const element = $('form input[name = "email"]');
+        element.setValue('aaa@gmail.com');
+
+    });
+    it('should fill Email field on Login page', () => {
+        const element = $('form input[name = "password"]');
+        element.setValue('11111');
+
+    });
+    it('should choose click button', () => {
+        const element = $('form button[type="submit"]').click();
+        browser.pause(3000);
+    });
+
+
+
+
 
 // проверить что страница логин login title
     //email
